41fcebcaa287d4596bad0a2bddfd80f3f828783f
sppawlik <spawlik07@gmail.com>
2024-11-05 12:11:15 +0100

    login

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
 import './index.css'
 import ArticlesDashboard from "./components/ArticlesDashboard";
+import {Authenticator} from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+import outputs from '../amplify_outputs.json';
+import {Amplify} from "aws-amplify";
+
+Amplify.configure(outputs);
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <div>
-      <ArticlesDashboard/>
-    </div>
-  </StrictMode>,
+    <StrictMode>
+        <Authenticator>
+            <ArticlesDashboard/>
+        </Authenticator>
+    </StrictMode>,
 )
