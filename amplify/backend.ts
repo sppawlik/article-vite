43cf5c8593a034c599c4b3c61e6735c54fb0d23c
sppawlik <spawlik07@gmail.com>
2024-11-11 10:35:18 +0100

    working without owner handlerer

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,11 +1,41 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
-import {data} from "./data/resource";
+import {data, listArticle} from "./data/resource";
+import {aws_dynamodb} from "aws-cdk-lib";
 
-/**
- * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
- */
-defineBackend({
+const backend = defineBackend({
   auth,
   data
 });
+
+const t = backend.data.resources.tables['UserArticle'];
+
+console.log('test2')
+
+
+const externalDataSourcesStack = backend.createStack("DynamoDataSources");
+
+
+const externalTable = aws_dynamodb.Table.fromTableName(
+    externalDataSourcesStack,
+    "DynamoDataSources",
+    "UserArticles"
+);
+
+
+backend.data.addDynamoDbDataSource(
+    "UserArticlesTableDataSource",
+    externalTable
+);
+
+//
+// const queryPolicyStatement = new PolicyStatement({
+//   effect: Effect.ALLOW,
+//   actions: ['dynamodb:Query'],
+//   resources: [
+//     t.tableArn,
+//     `${t.tableArn}/index/*`
+//   ]
+// });
+// console.log(backend.data.resources)
+// Add the policy to the function
