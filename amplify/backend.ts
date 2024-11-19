0d0947ed75ddc086d8497eb81a52030f2678ed30
sppawlik <spawlik07@gmail.com>
2024-11-19 12:00:58 +0100

    cleaning

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -4,46 +4,6 @@ import {data} from "./data/resource";
 import {aws_dynamodb} from "aws-cdk-lib";
 
 const backend = defineBackend({
-  auth,
-  data
+  // auth,
+  // data
 });
-
-const externalDataSourcesStack = backend.createStack("DynamoDataSources");
-
-const externalTable = aws_dynamodb.Table.fromTableName(
-    externalDataSourcesStack,
-    "DynamoDataSources",
-    "UserArticles"
-);
-
-backend.data.addDynamoDbDataSource(
-    "UserArticlesTableDataSource",
-    externalTable
-);
-
-
-const newsletterDataSourcesStack = backend.createStack("NewsletterDynamoDataSources");
-
-const newsletterTable = aws_dynamodb.Table.fromTableName(
-    newsletterDataSourcesStack,
-    "NewsletterDynamoDataSources",
-    "Newsletter"
-);
-
-backend.data.addDynamoDbDataSource(
-    "NewsletterTableDataSource",
-    newsletterTable
-);
-
-
-//
-// const queryPolicyStatement = new PolicyStatement({
-//   effect: Effect.ALLOW,
-//   actions: ['dynamodb:Query'],
-//   resources: [
-//     t.tableArn,
-//     `${t.tableArn}/index/*`
-//   ]
-// });
-// console.log(backend.data.resources)
-// Add the policy to the function
