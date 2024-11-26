7381769343f5d7aa21a859c8650ab453510f5397
sppawlik <spawlik07@gmail.com>
2024-11-26 14:43:51 +0100

    page title

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,6 @@ import {Authenticator} from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 import outputs from '../amplify_outputs.json';
 import {Amplify} from "aws-amplify";
-import {ApolloClient, ApolloProvider, InMemoryCache} from '@apollo/client';
 
 
 Amplify.configure(outputs);
