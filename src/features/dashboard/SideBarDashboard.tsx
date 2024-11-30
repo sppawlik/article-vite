90b6ac3cc7c5d7c970efef9f3e4b13992da6782d
sppawlik <spawlik07@gmail.com>
2024-11-30 10:04:23 +0100

    minimal width for main component

--- src/features/dashboard/SideBarDashboard.tsx
+++ src/features/dashboard/SideBarDashboard.tsx
@@ -118,7 +118,7 @@ export function SideBarDashboard() {
                 </nav>
             </aside>
             <div className="flex flex-col sm:gap-4 sm:py-4">
-                <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
+                <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8 min-w-[900px]">
                     {renderMainContent()}
                 </main>
             </div>
