4550174c7f6e0c810e6bc5a3a3b446aab5e9e743
sppawlik <spawlik07@gmail.com>
2025-03-26 18:01:13 +0100

    new endpoint

--- src/main.jsx
+++ src/main.jsx
@@ -21,7 +21,7 @@ Amplify.configure({
     },
     API: {
         GraphQL: {
-            endpoint: 'https://ncd6wcpyibcbvgosovogtpcdtq.appsync-api.eu-central-1.amazonaws.com/graphql',
+            endpoint: 'https://px5cfy5xh5ethnoo6ovqpm3num.appsync-api.eu-central-1.amazonaws.com/graphql',
             region: 'eu-central-1',
             defaultAuthMode: 'userPool'
         }
